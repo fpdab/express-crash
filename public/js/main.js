0fc6a47939ddcaeecf60574c137d8332bd26e403
fpdab <155249090+fpdab@users.noreply.github.com>
2024-05-20 21:23:24 +0200

    add form to frontend

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
 const output = document.querySelector("#output")
 const button = document.querySelector("#get-posts-btn")
+const form = document.querySelector("#add-post-form")
 
 // get posts and move them to output
 const getPosts = async () => {
   try {
-    const res = await fetch("http://localhost:8000/api/post")
+    const res = await fetch("http://localhost:8000/api/posts")
     if (!res.ok) {
       throw new Error("Fetching failed")
     }
@@ -20,5 +21,32 @@ const getPosts = async () => {
   }
 }
 
+// add post function
+async function addPost(e) {
+  e.preventDefault()
+  const formData = new FormData(this)
+  const title = formData.get("title")
+  try {
+    const res = await fetch("http://localhost:8000/api/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title }),
+    })
+    if (!res.ok) {
+      throw new Error("Failed adding post")
+    }
+    const newPost = await res.json()
+    const postEl = document.createElement("div")
+    postEl.textContent = newPost.title
+    output.appendChild(postEl)
+    getPosts()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 // event listener
 button.addEventListener("click", getPosts)
+form.addEventListener("submit", addPost)
